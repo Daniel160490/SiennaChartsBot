1bf0a2bab361f747225233c540f43548f55cb0a0
Dani <danigd71@gmail.com>
2025-02-06 19:44:39 +0100

    First commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
+
+TOKEN = "8166866319:AAH1OubVUZDaj12OQX6gv6nKJ6CtcawEYEg"
+
+async def start(update: Update, context):
+    await update.message.reply_text("¡Hola! Soy el bot de SiennaCharts.")
+
+async def bienvenida(update: Update, context):
+    for usuario in update.message.new_chat_members:
+        await update.message.reply_text(f"🎉 ¡Bienvenido/a, {usuario.first_name}! 🎉\nEsperamos que disfrutes en este grupo. No dudes en presentarte para que te conozcamos todos. ☺️")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, bienvenida))
+    print("SiennaCharts funcionando ...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
