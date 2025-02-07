af8d50c457c8fc9dcf5cd7bb99cef46852d2d0fb
-TΦM- <137113975+JTomas88@users.noreply.github.com>
2025-02-07 20:31:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,9 +69,7 @@ def main():
 
     print("SiennaCharts funcionando ...")
 
-    # Inicia la verificación dentro del loop de la aplicación
-    app.job_queue.run_repeating(lambda _: asyncio.create_task(enviar_posts_telegram(app)), interval=1800)
-
+    # Ejecuta el bot para recibir actualizaciones de Telegram
     app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
 
 if __name__ == "__main__":
